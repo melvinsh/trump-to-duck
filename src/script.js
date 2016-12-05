51fc04ba5ebe443277690e0e17d8347db76c9217
Melvin Lammerts <melvin@melvin.sh>
2016-12-05 13:57:44 +0100

    Fixed indentation

--- src/script.js
+++ src/script.js
@@ -2,31 +2,31 @@
 walk(document.body);
 
 function walk(node) {
-    var child, next;
+  var child, next;
 
-    switch (node.nodeType) {
-        case 1: // Element
-        case 9: // Document
-        case 11: // Document fragment
-            child = node.firstChild;
-            while (child) {
-                next = child.nextSibling;
-                walk(child);
-                child = next;
-            }
-            break;
-
-        case 3: // Text node
-            if (node.parentElement.tagName.toLowerCase() != "script") {
-                handleText(node);
-            }
-            break;
+  switch (node.nodeType) {
+    case 1: // Element
+    case 9: // Document
+    case 11: // Document fragment
+    child = node.firstChild;
+    while (child) {
+      next = child.nextSibling;
+      walk(child);
+      child = next;
     }
+    break;
+
+    case 3: // Text node
+    if (node.parentElement.tagName.toLowerCase() != "script") {
+      handleText(node);
+    }
+    break;
+  }
 }
 
 function handleText(textNode) {
-    var v = textNode.nodeValue;
-    v = v.replace(/Trump/, 'Duck');
-    v = v.replace(/TRUMP/, 'DUCK');
-    textNode.nodeValue = v;
+  var v = textNode.nodeValue;
+  v = v.replace(/Trump/, 'Duck');
+  v = v.replace(/TRUMP/, 'DUCK');
+  textNode.nodeValue = v;
 }
